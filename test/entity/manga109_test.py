fa3e679aa42f3e7c2403b6efc749caade91b6fe1
kur <rhdnfka94@gmail.com>
2020-07-22 11:26:31 +0900

    test: Assert #canonical_forms = # of img+xml files

--- test/entity/manga109_test.py
+++ test/entity/manga109_test.py
@@ -1,6 +1,14 @@
+from pathlib import Path
+
 from dw.api import make
 from dw.const import types
 from dw.entity.data import manga109
+from dw.util import file_utils as fu
 
 def test_make_canonical_form_data_from_manga109(m109):
-    make.data(manga109)(m109)
+    cfs = make.data(manga109)(m109)
+    img_dir = Path(m109, 'images')
+    xml_dir = Path(m109, 'manga109-annotations')
+    num_imgs = len(fu.descendants(img_dir))
+    num_xmls = len(fu.descendants(xml_dir))
+    assert len(cfs) == num_imgs + num_xmls
