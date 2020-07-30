57eb5f1e3518a239fbe161a7bdcaa0b7f088a9d8
kur <rhdnfka94@gmail.com>
2020-07-30 15:12:21 +0900

    feat: Generate tab/col names file

--- dw/db/schema.py
+++ dw/db/schema.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from uuid import uuid4
 from pprint import pformat
+from pathlib import Path
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
@@ -9,6 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects import postgresql as pg
 
 from dw.const import types
+from dw.util import fp
 
 #---------------------------------------------------------------
 # NOTE: If it is useless to general case, remove it!
@@ -35,7 +37,7 @@ class ReprHelper:
     
 Base = declarative_base(cls=ReprHelper)
 
-#---------------------------------------------------------------
+#===============================================================
 '''
 data는 서로 관계를 가질 수 있는 정보의 단위이다.
 named_dat_rel 은 이름 붙여진 데이터 사이의 관계이다.
@@ -66,8 +68,8 @@ class source(Base):
                   primary_key=True)
     name = Column(String, primary_key=True, nullable=False)
 
-class data_rel(Base):
-    __tablename__ = 'data_rel'
+class data_relation(Base):
+    __tablename__ = 'data_relation'
     aid = Column(pg.UUID(as_uuid=True), ForeignKey('data.uuid'),
                  primary_key=True)
     bid = Column(pg.UUID(as_uuid=True), ForeignKey('data.uuid'),
@@ -75,7 +77,7 @@ class data_rel(Base):
     type = Column(String, nullable=False) # extension
     
 def only_one_rel_rowseq(ids, type):
-    return (data_rel(aid=id, bid=id, type=type) for id in ids)
+    return (data_relation(aid=id, bid=id, type=type) for id in ids)
 
 class map_data_rel2rel_chunk(Base):
     __tablename__ = 'map_data_rel2rel_chunk'
@@ -88,7 +90,7 @@ class map_data_rel2rel_chunk(Base):
     out = Column(pg.UUID(as_uuid=True), primary_key=True)
 
     ForeignKeyConstraint(['inp',          'out'         ],
-                         ['data_rel.aid', 'data_rel.bid'])
+                         ['data_relation.aid', 'data_relation.bid'])
     
 def only_inp_chunk_rowseq(name, revision, ids):
     ''' 
@@ -114,16 +116,38 @@ def is_valid_column_name(name):
                         '_sa_class_manager',
                         '_decl_class_registry'}
 
-names = fp.go(
-    locals().values(),
-    fp.lfilter(lambda x: hasattr(x, '__tablename__')),
-    lambda classes: fp.concat(
+#---------------------------------------------------------------
+def generate_names_file(out_path=Path('dw/db/names.py')):
+    # locals -> classes
+    classes = fp.lfilter(
+        lambda x: hasattr(x, '__tablename__'),
+        globals().values())
+    
+    # classes -> code
+    all_nameseq = fp.concat(
         (cls.__name__ for cls in classes),
-        fp.mapcat(lambda cls:
-            fp.attr_names(cls,
-                          fp.every_pred(fp.is_public_name,
-                                        is_valid_column_name)),
-            classes
-        )),
-    set,
-    list)
+        fp.mapcat(
+            lambda cls: fp.attr_names(
+                cls, fp.every_pred(fp.is_public_name,
+                                   is_valid_column_name)),
+                classes
+        ))
+    names = list(set(all_nameseq))
+    code = '\n'.join([
+        "'''",
+        "Before import this, call schema.generate_name_file()",
+        "Or, import schema first.",
+        "This is auto-generated file. DO NOT MODIFY!",
+        "'''",
+        *("{} = '{}'".format(n,n) for n in names)
+    ])
+    
+    # code -> file
+    old_code =(out_path.read_text() if out_path.exists()
+               else None)
+    if code != old_code:
+        with open(out_path, 'w') as f:
+            f.write(code)
+        print('dw/db/names.py updated!')
+
+generate_names_file()
