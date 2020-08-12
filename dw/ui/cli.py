2dc425ee864f9356c24c85d8fb216edec4a5d3ad
kur <rhdnfka94@gmail.com>
2020-08-12 15:58:06 +0900

    Add cli.data szmc_v0

--- dw/ui/cli.py
+++ dw/ui/cli.py
@@ -117,3 +117,61 @@ class data(object):
         orm.init(conn)
         put.canonical_forms( make.data(old_snet)(root) )
         log.cli_cmd(conn, note)
+
+    def szmc_v0(self, conn, root, note=None):
+        '''
+        Add szmc_v0 data into db.
+        
+        szmc_v0 data is directory of files.
+        ROOT direcory must satisfy following structure.
+        map.json must be list of [old_name, some_path]
+        
+        .
+        ├── images  # (maybe) no text image. can be pngs.
+        │   ├── m0081.jpg
+        │   ├── m0082.jpg
+        │   ├── ...
+        │   └── m0100.jpg
+        ├── masks
+        │   ├── m0081.png
+        │   ├── m0082.png
+        │   ├── ...
+        │   └── m0100.png
+        ├── prev_images
+        │   ├── m0081.jpg
+        │   ├── m0082.jpg
+        │   ├── ...
+        │   └── m0100.jpg
+        └── tmp
+
+        [result] 
+        Cmd adds mask1bit folder and 1 bit pngs.
+        All 1 bit pngs are same name with masks.
+        .
+        ├── mask1bit #
+        │   ├── m0081.png
+        │   ├── m0082.png
+        │   ├── ...
+        │   └── m0100.png
+        
+        args: 
+        conn: connection str. 'id:pw@host:port/dbname' format.
+        root: root directory path string of data.
+        note: note for running command. If not None, it is logged.
+        '''
+        from dw.api import make, put
+        from dw.db import orm
+        from dw.entity.data import szmc_v0
+        from dw.ui import log
+
+        orm.init(conn)
+        
+        # Generate and Save 1 bit png masks
+        put.files(
+            make.data(szmc_v0.mask_file)(root),
+            exist_ok=True) # TODO: exist_ok=False?
+        # Put data into DB
+        put.canonical_forms(
+            make.data(szmc_v0)(root, False) )
+        
+        log.cli_cmd(conn, note)
