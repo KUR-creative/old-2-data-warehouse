68b155053c9bbc2233a0ba875cf192dc0e2a7be4
kur <rhdnfka94@gmail.com>
2020-08-13 16:10:00 +0900

    feat: Use main as cli / as testing bed

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import fire
+
 from dw.ui import cli
 
+
+if __name__ == '__main__':
+    fire.Fire(cli)
 '''
+from dw.ui import cli
+
 from dw.util.test_utils import env_val, skipif_none
+
+
 conn = snet = v0_m101 = v0_school = None
 conn = env_val(conn=conn)
 snet = env_val(snet=snet)
@@ -14,72 +23,6 @@ Q.DROP_ALL()
 assert cli.init(conn) == cli.RUN_SUCCESS
 # Add all data
 assert cli.data.old_snet(conn, snet) == cli.RUN_SUCCESS
-#assert cli.data.szmc_v0(conn, v0_m101) == cli.RUN_SUCCESS
-#assert cli.data.szmc_v0(conn, v0_school) == cli.RUN_SUCCESS
-'''
-
-import fire
-
-from dw.ui import cli
-
-
-if __name__ == '__main__':
-    fire.Fire(cli)
-    
-'''
-from dw.db import schema as S
-from dw.util import fp
-
-from pprint import pprint
-#print(N)
-#pprint(fp.pub_attr_names(N))
-for attr in fp.pub_attrs(S):
-    if hasattr(attr, '__tablename__'):
-        print(attr.__name__)
-
-        
-print("g:", globals(), 'l', locals(), 'v', vars())
-#pprint(locals())
-pprint(vars())
-
-S.generate_names_file()
-#pprint(S.asdf)
-#pprint(S.names)
-import os
-conn = os.environ['conn']
-m109 = os.environ['m109']
-orm.init(T.connection(conn))
-Q.DROP_ALL()
-Q.CREATE_TABLES()
-from dw.api import make, put
-from dw.entity.data import manga109
-
-cfs = make.data(manga109)(m109)
-put.data(cfs)
-
-
-with orm.session() as sess:
-    id = uuid4()
-    data = S.data(T.Data(id))
-    datums = [S.data(T.Data()), S.data(T.Data()),
-              S.data(T.Data()), S.data(T.Data())]
-    sess.add_all(datums)
-    sess.add(data)
-    sess.commit()
-    # ForeignKey가 되려면 먼저 commit을 해야하는 거 같음.
-    # 그러니까 data만 특수취급 해야 될 거 같은데? 더 해보고..
-    # data-rel도 문제 생길 수 있겠다.
-    # data -> annotation -> data-relation 이 순서로 해야 됨..
-    # 그러면 cf의 순서가 중요할 수 있겠는데..?
-    sess.add(S.file(uuid=data.uuid, path='ppap/bbab'))
-    sess.add(S.file(uuid=datums[0].uuid, path='ppap/bbab123'))
-    sess.commit()
-    result2 = sess.query(S.file)
-    result = sess.query(S.data) # select는 나중에..?
-from pprint import pprint
-print('===============>')
-pprint(list(result))
-pprint(list(result2))
-    
-Q.DROP_ALL()
+assert cli.data.szmc_v0(conn, v0_m101) == cli.RUN_SUCCESS
+assert cli.data.szmc_v0(conn, v0_school) == cli.RUN_SUCCESS
 '''
